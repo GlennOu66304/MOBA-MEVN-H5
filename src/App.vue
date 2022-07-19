c7c2d3703efcef7dbe192eba75e6f2a85ebd4a6c
Glenn Ou <2738328919@qq.com>
2022-07-19 17:28:36 +0800

    reset scss

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  created() {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <h1>This is Home Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  computed: {},
+};
+</script>
+
+<style scoped>
+.home {
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  box-sizing: border-box;
+}
+</style>
